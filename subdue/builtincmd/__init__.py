9e83bd4ddb8636c1ce196f17c7409c8f1ad56d8d
Jacobo de Vera <devel@jacobodevera.com>
2014-12-05 00:52:30 +0100

    Naive implementation of the commands built-in

--- subdue/builtincmd/__init__.py
+++ subdue/builtincmd/__init__.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import os
+
 registry = {}
 
 def built_in_command(name):
@@ -11,6 +13,7 @@ def built_in_command(name):
         c.name = name
         return c
     return wrapper
+
 
 class _BuiltInCommand(object):
     name = None
@@ -22,10 +25,33 @@ class _BuiltInCommand(object):
     def __call__(self):
         print ("built-in command: {} not yet implemented".format(self.name))
 
+
 @built_in_command('commands')
 class Commands(_BuiltInCommand):
+
     def __init__(self, args, paths):
         super(Commands, self).__init__(args, paths)
+
+    def __call__(self):
+        path = os.path.join(self.paths.commands, *self.args[1:])
+        if os.path.isdir(path):
+            commands = self.get_container_commands(path)
+            for command in commands:
+                print(command)
+            return 0
+        return 1
+
+    def get_container_commands(self, path):
+        commands = []
+        for path, dirs, files in os.walk(path):
+            commands += dirs
+            for f in files:
+                if os.access(os.path.join(path, f), os.X_OK):
+                    if f.startswith('sh-'):
+                        f = f[3:]
+                    commands.append(f)
+            break
+        return sorted(commands)
 
 
 @built_in_command('help')
